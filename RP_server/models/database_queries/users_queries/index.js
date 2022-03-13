7aaddb4a4bc38665e16c43adecba9dd91b302d5a
Raluca <rxa866@student.bham.ac.uk>
2022-03-13 19:29:35 +0000

    Deleted redundant db queries

--- RP_server/models/database_queries/users_queries/index.js
+++ RP_server/models/database_queries/users_queries/index.js
@@ -1,7 +1,5 @@
 exports.preRegisterUser = require("./PreRegisterUser");
 exports.registerUser = require("./RegisterUser");
-exports.preAuthenticateUser = require("./PreAuthenticateUser");
-exports.authenticateUser = require("./AuthenticateUser");
 exports.getUserChallenge = require("./GetUserChallenge");
 exports.getUser = require("./GetUser");
 exports.setUserCurrentChallenge = require("./setUserCurrentChallenge");
